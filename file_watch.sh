6279b4989973e9308ee26eeb9c81068efe17c9c5
a394szz <ngamell@mmm.com>
2019-06-12 10:52:29 -0500

    adding file_watch

---
+++ file_watch.sh
@@ -0,0 +1,21 @@
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from AWS...\n"
+  curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
+fi
+
+# install AWS Device SDK for Python if not already installed
+if [ ! -d ./aws-device-hackathon ]; then
+  printf "\nInstalling AWS SDK...\n"
+  git clone https://github.com/nga-27/aws-device-hackathon.git
+  pushd aws-device-hackathon
+  python setup.py install
+  popd
+fi
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+python aws-device-hackathon/samples/basicPubSub/basic_hackathon_1.py -e a26y2sst8zk5bt-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c e11f9f12f1.cert.pem -k e11f9f12f1.private.key
